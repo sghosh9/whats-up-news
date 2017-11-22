09f6561ccae17dd84d703351602dcfcd8cda9d5e
Sumo <somedutta.ghosh@innoraft.com>
2017-11-23 00:11:32 +0530

    displaying multiple objects in a table
    add multiple objects in an array to a table using a template through a view
    using dummy content

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ var SingleNews = Backbone.Model.extend({
   }
 });
 
-var AllNews = Backbone.View.extend({
+var AllNews = Backbone.Collection.extend({
   model: SingleNews
 });
+
+var NewsItems = [
+  {title: 'News 1', category: 'Food'},
+  {title: 'News 2', category: 'Movies'},
+  {title: 'News 3', category: 'Politics'},
+];
+
+
+var AllNewsView = Backbone.View.extend({
+  el: $('tbody'),
+  template: _.template($('#row-template').html()),
+  initialize: function() {
+    this.collection = new AllNews();
+
+    var self = this;
+    _.each(NewsItems, function(news) {
+      var NewsModel = new SingleNews(news);
+      self.collection.add(NewsModel);
+    });
+
+    this.render();
+
+    this.listenTo(this.collection, 'change', this.render);
+
+  },
+  render: function() {
+    var self = this;
+    _.each(this.collection.models, function(news) {
+      self.$el.append(self.template(news.attributes));
+    });
+  }
+});
+
+new AllNewsView;
